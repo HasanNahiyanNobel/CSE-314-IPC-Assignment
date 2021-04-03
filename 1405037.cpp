fb2e6b8d81ca6bfcdf1bd51b8b7acf88bf9d2d6a
Hasan Nahiyan Nobel <1405037.hn@ugrad.cse.buet.ac.bd>
2021-04-03 21:25:24 +0600

    Soft-coded and reorganized.

--- 1405037.cpp
+++ 1405037.cpp
@@ -2,6 +2,9 @@
 #include <cstdio>
 #include <thread>
 #include <queue>
+#define PRODUCTION_QUEUE_SIZE 200
+#define CHOCOLATE_CAKE_CHAR 'c'
+#define VANILLA___CAKE_CHAR '-'
 
 using namespace std;
 
@@ -19,6 +22,7 @@ int main () {
 	chef_x_thread.join();
 	chef_y_thread.join();
 
+	cout << endl;
 	ShowQueue(production_queue);
 	cout << "\nQueue size: " << queue_size << endl;
 
@@ -26,28 +30,27 @@ int main () {
 }
 
 void MakeChefXWork () {
-	for (int i=0; i<200; i++) {
-		production_queue.push('-');
+	for (int i=0; i<PRODUCTION_QUEUE_SIZE; i++) {
+		production_queue.push(CHOCOLATE_CAKE_CHAR);
 		queue_size++;
-		cout << "-";
+		cout << CHOCOLATE_CAKE_CHAR;
 	}
 }
 
 void MakeChefYWork () {
-	for (int i=0; i<200; i++) {
-		production_queue.push('v');
-		cout << "v";
+	for (int i=0; i<PRODUCTION_QUEUE_SIZE; i++) {
+		production_queue.push(VANILLA___CAKE_CHAR);
+		cout << VANILLA___CAKE_CHAR;
 		queue_size++;
 	}
 }
 
 void ShowQueue(queue<char> a_queue) {
 	queue<char> the_queue = a_queue;
-	cout << "\nFront";
+	cout << "Front-> ";
 	while (!the_queue.empty()) {
-		cout << ' ' << the_queue.front();
+		cout << the_queue.front();
 		the_queue.pop();
 	}
-	cout << "End";
-	cout << '\n';
+	cout << " ->End\n";
 }
