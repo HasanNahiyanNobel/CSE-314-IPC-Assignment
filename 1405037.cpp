b43a616034bc0ffd60f38789d5506e49819edc84
Hasan Nahiyan Nobel <1405037.hn@ugrad.cse.buet.ac.bd>
2021-04-04 20:57:41 +0600

    Introduced chef Z.

--- 1405037.cpp
+++ 1405037.cpp
@@ -14,15 +14,14 @@ const int kProductionQueueSize = 50;
 const char kChocolateCakeChar = 'c';
 const char kVanillaCakeChar = '-';
 const bool kIsMutexLockOn = true; // For debug purposes
-const int kSleepingTimeAfterEachConsoleOutput = 500; // In milliseconds
-const int kExecutionTimeLimit = 3000; // In milliseconds, though execution will continue if a thread has called GetExecutionTime() before reaching that time limit.
+const int kSleepingTimeAfterEachConsoleOutput = 0; // In milliseconds
+const int kExecutionTimeLimit = 10000; // In milliseconds, though execution will continue if a thread has called GetExecutionTime() before reaching that time limit.
 
 /// Global variables
 queue<char> production_queue;
 int queue_size = 0;
 pthread_mutex_t mutex;
 auto start = high_resolution_clock::now();
-auto stop = high_resolution_clock::now();
 
 /// Function declarations
 void * MakeChefXWork(void *pVoid);
@@ -30,19 +29,21 @@ void * MakeChefYWork(void *pVoid);
 void * MakeChefZWork(void *pVoid);
 void ShowQueue(queue<char> a_queue);
 void SleepABit(int sleeping_time);
-bool PromptForTheNextActionOrExit();
 int GetExecutionTime();
 
 /// Main function
 int main () {
 	pthread_t chef_x_thread;
 	pthread_t chef_y_thread;
+	pthread_t chef_z_thread;
 
-	pthread_create(&chef_x_thread, NULL, MakeChefXWork, NULL);
-	pthread_create(&chef_y_thread, NULL, MakeChefYWork, NULL);
+	pthread_create(&chef_x_thread, nullptr, MakeChefXWork, nullptr);
+	pthread_create(&chef_y_thread, nullptr, MakeChefYWork, nullptr);
+	pthread_create(&chef_z_thread, nullptr, MakeChefZWork, nullptr);
 
-	pthread_join(chef_x_thread, NULL);
-	pthread_join(chef_y_thread, NULL);
+	pthread_join(chef_x_thread, nullptr);
+	pthread_join(chef_y_thread, nullptr);
+	pthread_join(chef_z_thread, nullptr);
 
 	cout << "Queue size: " << queue_size << endl;
 	cout << "Execution ended in: " << GetExecutionTime() << "ms" << endl;
@@ -54,13 +55,13 @@ int main () {
 void * MakeChefXWork (void *pVoid) {
 	while (GetExecutionTime() < kExecutionTimeLimit) {
 		if (kIsMutexLockOn) pthread_mutex_lock(&mutex); // Critical section starts
-		if (queue_size < kProductionQueueSize) {
+		if (production_queue.size() < kProductionQueueSize) {
 			production_queue.push(kChocolateCakeChar);
 			queue_size++;
 
 			for (int j=0; j<5000; j++) {queue_size++;queue_size--;} // To get a clearer view of race condition
 
-			cout << "New cake by ChefX; production_queue: ";
+			cout << "New cake by Chef X; production_queue:\t";
 			ShowQueue(production_queue);
 			cout << endl;
 			SleepABit(kSleepingTimeAfterEachConsoleOutput);
@@ -73,13 +74,33 @@ void * MakeChefXWork (void *pVoid) {
 void * MakeChefYWork (void *pVoid) {
 	while (GetExecutionTime() < kExecutionTimeLimit) {
 		if (kIsMutexLockOn) pthread_mutex_lock(&mutex); // Critical section starts
-		if (queue_size < kProductionQueueSize) {
+		if (production_queue.size() < kProductionQueueSize) {
 			production_queue.push(kVanillaCakeChar);
 			queue_size++;
 
 			for (int j=0; j<5000; j++) {queue_size++;queue_size--;} // To get a clearer view of race condition
 
-			cout << "New cake by ChefY; production_queue: ";
+			cout << "New cake by Chef Y; production_queue:\t";
+			ShowQueue(production_queue);
+			cout << endl;
+			SleepABit(kSleepingTimeAfterEachConsoleOutput);
+		}
+		if (kIsMutexLockOn) pthread_mutex_unlock(&mutex); // Critical section ends
+	}
+	return nullptr;
+}
+
+void * MakeChefZWork (void *pVoid) {
+	while (GetExecutionTime() < kExecutionTimeLimit) {
+		if (kIsMutexLockOn) pthread_mutex_lock(&mutex); // Critical section starts
+		if (production_queue.size() > 0) {
+			char a_cake = production_queue.front();
+			production_queue.pop();
+			queue_size--;
+
+			for (int j=0; j<5000; j++) {queue_size++;queue_size--;} // To get a clearer view of race condition
+
+			cout << "Cake taken by Chef Z; production_queue:\t";
 			ShowQueue(production_queue);
 			cout << endl;
 			SleepABit(kSleepingTimeAfterEachConsoleOutput);
@@ -90,6 +111,9 @@ void * MakeChefYWork (void *pVoid) {
 }
 
 void ShowQueue (queue<char> a_queue) {
+	if (a_queue.size()==0) {
+		return;
+	}
 	queue<char> the_queue = a_queue;
 
 	//Output the first element, without a preceding space
@@ -105,16 +129,6 @@ void ShowQueue (queue<char> a_queue) {
 
 void SleepABit (int sleeping_time) {
 	this_thread::sleep_for(milliseconds{sleeping_time});
-}
-
-bool PromptForTheNextActionOrExit () {
-	cout << "Press enter to continue, E to exit...\n";
-	if (cin.get() == 'E') {
-		exit(0);
-	}
-	else {
-		return true;
-	}
 }
 
 int GetExecutionTime () {
