145cf8805c11f20b1c7dafb6c4b3db02ac6aa842
Hasan Nahiyan Nobel <1405037.hn@ugrad.cse.buet.ac.bd>
2021-04-04 12:36:12 +0600

    Introduced pthread instead of thread. Changed macros to constant variables.

--- 1405037.cpp
+++ 1405037.cpp
@@ -1,51 +1,53 @@
 #include <iostream>
 #include <cstdio>
-#include <thread>
+#include <pthread.h>
 #include <queue>
-#define PRODUCTION_QUEUE_SIZE 200
-#define CHOCOLATE_CAKE_CHAR 'c'
-#define VANILLA___CAKE_CHAR '-'
 
 using namespace std;
+
+const int kProductionQueueSize = 200;
+const char kChocolateCakeChar = 'c';
+const char kVanillaCakeChar = '-';
 
 queue<char> production_queue;
 int queue_size = 0;
 
-void MakeChefXWork ();
-void MakeChefYWork ();
+void * MakeChefXWork(void *pVoid);
+void * MakeChefYWork(void *pVoid);
 void ShowQueue(queue<char> a_queue);
 
 int main () {
-	thread chef_x_thread(MakeChefXWork);
-	thread chef_y_thread(MakeChefYWork);
+	pthread_t chef_x_thread;
+	pthread_t chef_y_thread;
 
-	chef_x_thread.join();
-	chef_y_thread.join();
+	pthread_create(&chef_x_thread, NULL, MakeChefXWork, NULL);
+	pthread_create(&chef_y_thread, NULL, MakeChefYWork, NULL);
 
-	cout << endl;
-	ShowQueue(production_queue);
-	cout << "\nQueue size: " << queue_size << endl;
+	pthread_join(chef_x_thread, NULL);
+	pthread_join(chef_y_thread, NULL);
 
 	return 0;
 }
 
-void MakeChefXWork () {
-	for (int i=0; i<PRODUCTION_QUEUE_SIZE; i++) {
-		production_queue.push(CHOCOLATE_CAKE_CHAR);
+void * MakeChefXWork (void *pVoid) {
+	for (int i=0; i < kProductionQueueSize; i++) {
+		production_queue.push(kChocolateCakeChar);
 		queue_size++;
-		cout << CHOCOLATE_CAKE_CHAR;
+		cout << kChocolateCakeChar;
 	}
+	return nullptr;
 }
 
-void MakeChefYWork () {
-	for (int i=0; i<PRODUCTION_QUEUE_SIZE; i++) {
-		production_queue.push(VANILLA___CAKE_CHAR);
-		cout << VANILLA___CAKE_CHAR;
+void * MakeChefYWork (void *pVoid) {
+	for (int i=0; i < kProductionQueueSize; i++) {
+		production_queue.push(kVanillaCakeChar);
+		cout << kVanillaCakeChar;
 		queue_size++;
 	}
+	return nullptr;
 }
 
-void ShowQueue(queue<char> a_queue) {
+void ShowQueue (queue<char> a_queue) {
 	queue<char> the_queue = a_queue;
 	cout << "Front-> ";
 	while (!the_queue.empty()) {
