4f02dede7f731cf8e3d092155069c4697ffb7819
Hasan Nahiyan Nobel <1405037.hn@ugrad.cse.buet.ac.bd>
2021-04-05 14:05:39 +0600

    Better visualization added. Tidied up stuffs a bit too.

--- sample_code.cpp
+++ sample_code.cpp
@@ -1,10 +1,9 @@
-#include<cstdio>
-#include<pthread.h>
-#include<semaphore.h>
-#include<queue>
-#include <unistd.h>
 #include <iostream>
 #include <chrono>
+#include <queue>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
 
 
 /// Namespace
@@ -14,9 +13,9 @@ using namespace chrono;
 
 /// Constants
 const int kProductionQueueSize = 5;
-const int kNumberOfProductions = 10;
 const char kChocolateCakeChar = 'c';
 const char kVanillaCakeChar = 'v';
+const string kProductionQueueName = "Queue 1";
 const int kSleepingTimeAfterEachConsoleOutput = 1; // In seconds
 const int kApproximatedExecutionTimeLimit = 30; // In seconds, though execution will continue if a thread has called GetExecutionTime() before reaching that time limit. Numerically if there are n threads, execution seems to continue (n-1) seconds more.
 auto kStartTime = high_resolution_clock::now();
@@ -27,6 +26,8 @@ sem_t empty;
 sem_t full;
 pthread_mutex_t pthreadMutex;
 queue<char> production_queue;
+queue<char> chocolate_queue;
+queue<char> vanilla_queue;
 
 
 /// Function definitions
@@ -34,6 +35,7 @@ void InitSemaphore();
 void* StartChefX(void* pVoid);
 void* StartChefY(void* pVoid);
 void* StartChefZ(void* pVoid);
+void PrintQueue(queue<char> the_queue, const string& name, int max_size);
 int GetExecutionTime();
 
 
@@ -69,15 +71,16 @@ void InitSemaphore () {
 }
 
 void* StartChefX (void* pVoid) {
-	int i;
 	for(; GetExecutionTime() < kApproximatedExecutionTimeLimit;) {
 		sem_wait(&empty);
 		pthread_mutex_lock(&pthreadMutex);
 
-		sleep(kSleepingTimeAfterEachConsoleOutput);
 		production_queue.push(kChocolateCakeChar);
-		cout << "Chef X produced a cake; queue size: " << production_queue.size() << endl;
-		//printf("Chef X produced item %d\n",i);
+
+		cout << "Chef X produced a *chocolate* cake; queue size: " << production_queue.size() << endl;
+		PrintQueue(production_queue, kProductionQueueName, kProductionQueueSize);
+
+		sleep(kSleepingTimeAfterEachConsoleOutput);
 
 		pthread_mutex_unlock(&pthreadMutex);
 		sem_post(&full);
@@ -86,15 +89,16 @@ void* StartChefX (void* pVoid) {
 }
 
 void* StartChefY (void* pVoid) {
-	int i;
 	for(; GetExecutionTime() < kApproximatedExecutionTimeLimit;) {
 		sem_wait(&empty);
 		pthread_mutex_lock(&pthreadMutex);
 
-		sleep(kSleepingTimeAfterEachConsoleOutput);
 		production_queue.push(kVanillaCakeChar);
-		cout << "Chef Y produced a cake; queue size: " << production_queue.size() << endl;
-		//printf("Chef Y produced item %d\n",i);
+
+		cout << "Chef Y produced a *vanilla* cake; queue size: " << production_queue.size() << endl;
+		PrintQueue(production_queue, kProductionQueueName, kProductionQueueSize);
+
+		sleep(kSleepingTimeAfterEachConsoleOutput);
 
 		pthread_mutex_unlock(&pthreadMutex);
 		sem_post(&full);
@@ -103,26 +107,41 @@ void* StartChefY (void* pVoid) {
 }
 
 void* StartChefZ (void* pVoid) {
-	int i;
 	for(; GetExecutionTime() < kApproximatedExecutionTimeLimit;) {
 		sem_wait(&full);
 		pthread_mutex_lock(&pthreadMutex);
 
-		sleep(kSleepingTimeAfterEachConsoleOutput);
 		char cake = production_queue.front();
 		production_queue.pop();
-		if (cake==kChocolateCakeChar) {
-			cout << "Chef Z took a *chocolate* cake; queue size: " << production_queue.size() << endl;
-		}
-		else {
-			cout << "Chef Z took a *vanilla* cake; queue size: " << production_queue.size() << endl;
-		}
-		//printf("Chef Z consumed cake %d\n", cake);
+
+		string cake_name;
+		if (cake==kChocolateCakeChar) cake_name = "chocolate";
+		else cake_name = "vanilla";
+		cout << "Chef Z took a *"<< cake_name <<"* cake; queue size: " << production_queue.size() << endl;
+		PrintQueue(production_queue, kProductionQueueName, kProductionQueueSize);
+
+		sleep(kSleepingTimeAfterEachConsoleOutput);
 
 		pthread_mutex_unlock(&pthreadMutex);
 		sem_post(&empty);
 	}
 	return nullptr;
+}
+
+void PrintQueue (queue<char> the_queue, const string& name, int max_size) {
+	if (!name.empty()) {
+		cout << name << ": ";
+	}
+	for (int i=0; i<max_size; i++) {
+		if (the_queue.empty()) {
+			cout << "- ";
+		}
+		else {
+			cout << the_queue.front() << " ";
+			the_queue.pop();
+		}
+	}
+	cout << endl << endl;
 }
 
 /**
